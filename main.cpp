1e7a1c6bf215ddcd96d655c759c4288b1a6cccc8
Pakibabes <navalcommander8@gmail.com>
2026-03-12 02:24:17 +0800

    Second Commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "interpreter.h"
 using namespace std;
 
-int main(){
 
+vector<string> readFile(string fName){
+    vector<string> lines;
+    ifstream file(fName);
+    string line;
+
+    while(getline(file, line)){
+        lines.push_back(line);
+    }
+
+    return lines;
+}
+int main(){
     cout << "Lexor Programming";
-    
+    vector<string> program = readFile("test_ni_Pakibabes.lexor");
     return 0;
 }
